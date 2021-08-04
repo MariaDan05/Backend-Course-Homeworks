d664558575f71783dd85e0a587e4e654bb1a1d80
MariaDan05 <54112953+MariaDan05@users.noreply.github.com>
2021-08-04 12:23:26 +0300

    Update ServerSideMain.java

--- simpleJavaHttpServer/com.picsart/server/ServerSideMain.java
+++ simpleJavaHttpServer/com.picsart/server/ServerSideMain.java
@@ -9,18 +9,20 @@ public class ServerSideMain {
     public static final int PORT = 8008;
     public static LinkedList<ServerProcessThread> serverList = new LinkedList<>();
 
-    public static void main(String[] args) throws IOException {
-        try (ServerSocket server = new ServerSocket(PORT)) {
-            System.out.println("Server Started");
-            while (true) {
-                Socket socket = server.accept();
-                RequestHandler.handleRequest(socket);
-                try {
-                    serverList.add(new ServerProcessThread(socket));
-                } catch (IOException e) {
-                    socket.close();
+    public static void main(String[] args) {
+        try {
+            try (ServerSocket server = new ServerSocket(PORT)) {
+                System.out.println("Server Started");
+                while (true) {
+                    Socket socket = server.accept();
+                    RequestHandler.handleRequest(socket);
+                    try {
+                        serverList.add(new ServerProcessThread(socket));
+                    } catch (IOException e) {
+                        socket.close();
+                    }
                 }
             }
-        }
+        }catch (IOException ignored) {}
     }
 }
