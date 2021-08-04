51de07c325a0070cdb1d8e5e84e39b56f8b8eac7
MariaDan05 <54112953+MariaDan05@users.noreply.github.com>
2021-08-04 11:49:31 +0300

    Create ServerSideMain.java

---
+++ simpleJavaHttpServer/com.picsart/server/ServerSideMain.java
@@ -0,0 +1,26 @@
+package com.picsart.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedList;
+
+public class ServerSideMain {
+    public static final int PORT = 8008;
+    public static LinkedList<ServerProcessThread> serverList = new LinkedList<>();
+
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket server = new ServerSocket(PORT)) {
+            System.out.println("Server Started");
+            while (true) {
+                Socket socket = server.accept();
+                RequestHandler.handleRequest(socket);
+                try {
+                    serverList.add(new ServerProcessThread(socket));
+                } catch (IOException e) {
+                    socket.close();
+                }
+            }
+        }
+    }
+}
